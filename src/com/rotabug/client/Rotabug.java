9ad4f5fb060fe93694c6bf2e51c8ed3b3d8f6c9e
David Berry <d.berry@jach.hawaii.edu>
2012-10-28 16:19:32 +0000

    wip

--- src/com/rotabug/client/Rotabug.java
+++ src/com/rotabug/client/Rotabug.java
@@ -1,125 +1,113 @@
 package com.rotabug.client;
 
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.rotabug.client.presenter.HomePresenter;
+import com.rotabug.client.presenter.SignInPresenter;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class Rotabug implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
+	// The application name as it appears in text seen by the user.
+	public static final String APP_NAME = "Rotabug";
 
-	/**
-	 * This is the entry point method.
-	 */
+	// The logo
+	public static final String LOGO_PNG = "Rotabug.png";
+	public static final String LOGO_WIDTH = "100px";
+	public static final String LOGO_HEIGHT = "94px";
+
+	// Indicates how verbose we should be. Zero is for normal user-level
+	// verbosity. Higher values produce more verbosity.
+	public static final int DEBUG_LEVEL = 1;
+
+	// Main entry. This follows the architecture described at
+	// https://developers.google.com/web-toolkit/articles/mvp-architecture
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// Create an object to communicate with the server
+		ServerRequester server = new ServerRequester();
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		// Create an object for displaying dialog boxes for user interaction.
+		UserRequester user = UserRequester.getInstance();
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+		// Create an object for sending events from one presenter to another.
+		HandlerManager eventBus = new HandlerManager(null);
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+		// This the main class for creating and controlling the GUI.
+		AppController appViewer = new AppController(server, user, eventBus);
 
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+		// Get the outer most panel which contains the headers, footers, etc,
+		// plus the application GUI.
+		RootPanel root = RootPanel.get(APP_NAME);
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
+		// Create the header panel, add it to the root, and set its CSS ID to
+		// "header".
+		HorizontalPanel banner = new HorizontalPanel();
+		root.add(banner);
+		setID(banner, "banner");
 
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
+		// Add the logo to the left end of the header, followed by headline
+		// text.
+		Image logo = new Image(LOGO_PNG);
+		setID(logo, "banner-logo");
+		banner.add(logo);
+		banner.setCellWidth(logo, LOGO_WIDTH);
 
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
+		HTML title = new HTML(
+				"<big><b>Rotabug -</b></big><br>rota creation made easy");
+		setID(title, "banner-title");
+		banner.add(title);
 
+		// Add links at the right end of the banner for sign-up, login and home.
+		VerticalPanel links = new VerticalPanel();
+		EventLink link = new EventLink("Sign in",SignInPresenter.PRESENT);
+		link.addStyleName("banner-link");
+		link.setTitle("Sign in using a new or existing account");
+		links.add(link);
 
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-			}
-		}
+		Hyperlink linka = new Hyperlink("Home", HomePresenter.PLACE);
+		linka.setStyleName("banner-link");
+		linka.setTitle("Go to the " + APP_NAME + " home page");
+		links.add(linka);
+		//
+		// link = new Hyperlink("Log in", LogInPresenter.PLACE);
+		// link.setStyleName( "banner-link" );
+		// link.setTitle("Log into your "+APP_NAME+" account");
+		// links.add(link);
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		banner.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+		banner.add(links);
+
+		// Create a panel for the application GUI, set its CSS ID and add it to
+		// the root panel.
+		VerticalPanel appUI = new VerticalPanel();
+		setID(appUI, "appUI");
+		root.add(appUI);
+
+		// Display the main home page for the app.
+		appViewer.go(appUI);
+
+	}
+
+	// Set the ID for a widget
+	public static void setID(Widget w, String id) {
+		DOM.setElementAttribute(w.getElement(), "id", id);
+	}
+
+	// Add an attribute setting to an element's style tag.
+	public static void addStyleAttribute(Widget w, String attr) {
+		Element el = w.getElement();
+		String style = DOM.getElementAttribute(el, "style");
+		style = style + attr;
+		DOM.setElementAttribute(el, "style", style);
 	}
 }
